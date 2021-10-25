4653d7757abba8e016633b40e917e0e523dee04f
LarisaGimbu <gimbularisa@gmail.com>
2021-10-25 15:21:30 +0200

    divisibili per 5 e 3

--- js/script.js
+++ js/script.js
@@ -9,11 +9,23 @@
 
 const row = document.querySelector('.row');
 
-for (let i = 0; i <= 100; i++){
+for (let i = 1; i <= 100; i++){
   const box = document.createElement('div');
   box.className = 'box';
   row.append(box);
-  box.innerHTML = i + 1;
+  box.innerHTML = i;
+  
+  if(!(i % 3)){
+    box.classList.add('green');
+    box.innerHTML = 'fizz';
+  }else if(!(i % 5)){
+    box.classList.add('yellow');
+    box.innerHTML = 'buzz';
+  }
+  if(!(i % 3) && !(i % 5)){
+    box.classList.add('red');
+    box.innerHTML = 'fizzbuzz';
+  }
   
 
 }
